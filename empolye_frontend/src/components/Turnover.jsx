691ffd7f5e271b155415d12b2604dd94e273a3dc
ifla2003 <148528158+ifla2003@users.noreply.github.com>
2024-03-20 18:18:01 +0530

    Update Turnover.jsx

--- empolye_frontend/src/components/Turnover.jsx
+++ empolye_frontend/src/components/Turnover.jsx
@@ -1,34 +1,12 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Employee from "./Employee";
+import List from "./List";
 
 
 const Turnover = () => {
   const navigate = useNavigate("");
   const [employeeData, setOverData] = useState([]);
-  const [pnumber, setPNumber] = useState("");
-  const [pname, setPName] = useState("");
-  const [buyingprice, setBuyingPrice] = useState("");
-  const [bought, setBought] = useState("");
-  const [amountout, setAmountOut] = useState("");
-  const [sellingprice, setSellingPrice] = useState("");
-  const [sold, setSold] = useState("");
-  const [stock, setStock] = useState("");
-  const [unitprofit, setUnitProfit] = useState("");
-  const [profit, setProfit] = useState("");
-
-  useEffect(() => {
-   axios
-     .get("http://localhost:3001/employeeData")
-     .then((response) => {
-      setOverData(response.data);
-      // console.log(response.data);
-     })
-     .catch((error) => {
-       console.log(error);
-     });
- }, [employeeData]);
 
   useEffect(() => {
     axios
@@ -39,46 +17,66 @@ const Turnover = () => {
       .catch((error) => {
         console.log(error);
       });
+     
   }, [employeeData]);
-  {
-   console.log(employeeData)
-   
-    employeeData.map((employee) => {
-      return (
-         //  <List
-         //    key={employee.id}
-         //    id={employee.id}
-         //   pnumber={employee.pnumber}
-         //    pname={employee.pname}
-         //    buyingprice={employee.buyingprice}
-         //    bought={employee.bought}
-         //    amountout={employee.amountout}
-         //    sellingprice={employee.sellingprice}
-         //    sold={employee.sold}
-         //    stock={employee.stock}
-         //    unitprofit={employee.unitprofit}
-         //    profit={employee.profit}
 
-         //  />
+  return(
+<>
+    <button
+    onClick={() => {
+      navigate("/AddData");
+    }}
+  >
+    Add Data
+  </button>
+    
+   
+    <div className="employeeList">
+      <div className="employeeData">
+        <div>id</div>
+        <div> pnumber </div>
+        <div>pname </div>
+        <div> buyingprice </div>
+        <div> bought </div>
+        <div> amountout </div>
+        <div> sellingprice </div>
+        <div> sold </div>
+        <div> stock </div>
+        <div> unitprofit </div>
+        <div> profit </div>
 
-        <div className="employeeData">
-          {/* <div>{id}</div> */}
-          <div> {pnumber} </div>
-          <div>{pname}</div>
-          <div> {buyingprice} </div>
-          <div> {bought} </div>
-          <div> {amountout} </div>
-          <div> {sellingprice} </div>
-          <div> {sold}</div>
-          <div> {stock} </div>
-          <div> {unitprofit} </div>
-          <div> {profit}</div>
-        </div>
-      );
-    });
+        {/* <div>total salary</div> */}
+      </div>
+      {employeeData.map((employee) => {
+       
+        return (  
+          <List
+            key={employee.id}
+            id={employee.id}
+           pnumber={employee.pnumber}
+            pname={employee.pname}
+            buyingprice={employee.buyingprice}
+            bought={employee.bought}
+            amountout={employee.amountout}
+            sellingprice={employee.sellingprice}
+            sold={employee.sold}
+            stock={employee.stock}
+            unitprofit={employee.unitprofit}
+            profit={employee.profit}
+
+          />
+        );
+      })}
+      {/* <div>
+        <div className="s1"> Total amountout = { amountout} </div>
+        <div className="s2"> Total unitprofit = {unitprofit} </div>
+        <div className="s3"> Total profit = {profit} </div>
+      </div> */}
+    </div>
    
-  }
-   
-};
+    </>
+);
+    }
+  
 
 export default Turnover;
